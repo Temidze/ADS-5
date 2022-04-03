1701db8541c8a66cdfc3c28b04af4d312625a7fd
Temidze <94217293+Temidze@users.noreply.github.com>
2022-04-03 15:55:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,59 +1,55 @@
-// Copyright 2021 NNTU-CS
 #include <string>
 #include <map>
 #include "tstack.h"
-
-int HelpF(char Th) {
-    switch (Th)
-    {
+int pr(char ch) {
+  switch (ch) {
     case '(':
-        return 0;
+      return 0;
     case ')':
-        return 1;
+      return 1;
     case '+':
-        return 2;
+      return 2;
     case '-':
-        return 2;
+      return 2;
     case '*':
-        return 3;
+      return 3;
     case '/':
-        return 3;
+      return 3;
     case ' ':
-        return 4;
+      return 4;
     default:
-        return 5;
-    }
-    return -1;
+      return 5;
+  }
+  return -1;
 }
-
 std::string infx2pstfx(std::string inf) {
   TStack<char, 100> stack;
   std::string output;
   int i = 0;
   for (; i < inf.length(); i++) {
-    if (5 == HelpF(inf[i])) {
+    if (5 == pr(inf[i])) {
       output += inf[i];
-      while (5 == HelpF(inf[i + 1]) && (i + 1) < inf.length()) {
+      while (5 == pr(inf[i + 1]) && (i + 1) < inf.length()) {
         output += inf[i + 1];
         i += 1;
       }
       output += ' ';
     } else {
-      if (0 == HelpF(inf[i])) {
+      if (0 == pr(inf[i])) {
         stack.push(inf[i]);
-      } else if (HelpF(inf[i]) > HelpF(stack.get())) {
+      } else if (pr(inf[i]) > pr(stack.get())) {
         stack.push(inf[i]);
       } else if (stack.isEmpty()) {
         stack.push(inf[i]);
-      } else if (1 == HelpF(inf[i])) {
-        while (0 != HelpF(stack.get())) {
+      } else if (1 == pr(inf[i])) {
+        while (0 != pr(stack.get())) {
           output += stack.get();
           output += ' ';
           stack.pop();
         }
         stack.pop();
-      } else if (HelpF(inf[i]) <= HelpF(stack.get())) {
-        while (!stack.isEmpty() && 1 < HelpF(stack.get())) {
+      } else if (pr(inf[i]) <= pr(stack.get())) {
+        while (!stack.isEmpty() && 1 < pr(stack.get())) {
           output += stack.get();
           output += ' ';
           stack.pop();
@@ -70,42 +66,40 @@ std::string infx2pstfx(std::string inf) {
   output.pop_back();
   return output;
 }
-
 int eval(std::string pref) {
-    TStack<int, 100> stackT;
-    std::string output;
-    int c = 0;
-    for (int i = 0; i < pref.length(); i++) {
-        std::string check = "";
-        while (5 == pr(pref[i])) {
-            check += pref[i];
-            i += 1;
-        }
-        if (check != "") {
-            stackT.push(std::stoi(check));
-        }
-        if (2 == pr(pref[i]) || 3 == pr(pref[i])) {
-            int a = stackT.get();
-            stackT.pop();
-            int b = stackT.get();
-            stackT.pop();
-            if ('-' == pref[i]) {
-                c = b - a;
-                stackT.push(c);
-            }
-            else if ('+' == pref[i]) {
-                c = a + b;
-                stackT.push(c);
-            }
-            else if ('*' == pref[i]) {
-                c = a * b;
-                stackT.push(c);
-            }
-            else if ('/' == pref[i]) {
-                c = b / a;
-                stackT.push(c);
-            }
-        }
+  TStack<int, 100> istack;
+  std::string output;
+  int result = 0;
+  int i = 0;
+  for (; i < pref.length(); i++) {
+    std::string check = "";
+    while (5 == pr(pref[i])) {
+      check += pref[i];
+      i += 1;
     }
-    return stackT.get();
+    if (check != "") {
+      istack.push(std::stoi(check));
+    }
+    if (2 == pr(pref[i]) || 3 == pr(pref[i])) {
+      int a = istack.get();
+      istack.pop();
+      int b = istack.get();
+      istack.pop();
+      if ('-' == pref[i]) {
+        result = b - a;
+        istack.push(result);
+      } else if ('+' == pref[i]) {
+        result = a + b;
+        istack.push(result);
+      } else if ('*' == pref[i]) {
+        result = a * b;
+        istack.push(result);
+      } else if ('/' == pref[i]) {
+        result = b / a;
+        istack.push(result);
+      }
+    }
+  }
+  result = istack.get();
+  return result;
 }
