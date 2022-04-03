5d6805204ef237181011c4fda6ccf9e3a07d8407
Temidze <94217293+Temidze@users.noreply.github.com>
2022-04-03 21:11:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,106 +1,119 @@
 // Copyright 2021 NNTU-CS
-#include <string>
-#include <map>
-#include "tstack.h"
-int pr(char ch) {
-  switch (ch) {
+int HelpF(char Th) {
+    switch (Th)
+    {
     case '(':
-      return 0;
+        return 0;
+        break;
     case ')':
-      return 1;
+        return 1;
+        break;
     case '+':
-      return 2;
+        return 2;
+        break;
     case '-':
-      return 2;
+        return 2;
+        break;
     case '*':
-      return 3;
+        return 3;
+        break;
     case '/':
-      return 3;
+        return 3;
+        break;
     case ' ':
-      return 4;
+        return 4;
     default:
-      return 5;
-  }
-  return -1;
+        return 5;
+    }
+    return -1;
 }
+
 std::string infx2pstfx(std::string inf) {
-  TStack<char, 100> stack;
-  std::string output;
-  int i = 0;
-  for (; i < inf.length(); i++) {
-    if (5 == pr(inf[i])) {
-      output += inf[i];
-      while (5 == pr(inf[i + 1]) && (i + 1) < inf.length()) {
-        output += inf[i + 1];
-        i += 1;
-      }
-      output += ' ';
-    } else {
-      if (0 == pr(inf[i])) {
-        stack.push(inf[i]);
-      } else if (pr(inf[i]) > pr(stack.get())) {
-        stack.push(inf[i]);
-      } else if (stack.isEmpty()) {
-        stack.push(inf[i]);
-      } else if (1 == pr(inf[i])) {
-        while (0 != pr(stack.get())) {
-          output += stack.get();
-          output += ' ';
-          stack.pop();
+    TStack<char, 100> stackO;
+    std::string out;
+    for (int i = 0; i < inf.length(); i++) {
+        //циферки
+        if (5 == HelpF(inf[i])) {
+            out += inf[i];
+            while (5 == HelpF(inf[i + 1]) && (i + 1) < inf.length()) {
+                out += inf[i + 1];
+                i += 1;
+            }
+            out += ' ';
         }
-        stack.pop();
-      } else if (pr(inf[i]) <= pr(stack.get())) {
-        while (!stack.isEmpty() && 1 < pr(stack.get())) {
-          output += stack.get();
-          output += ' ';
-          stack.pop();
+        //Не циферки как не странно
+        else {
+            if (HelpF(inf[i]) == 0) {
+                stackO.push(inf[i]);
+            }
+            else if (HelpF(inf[i]) > HelpF(stackO.get())) {
+                stackO.push(inf[i]);
+            }
+            else if (stackO.isEmpty()) {
+                stackO.push(inf[i]);
+            }
+            else if (HelpF(inf[i]) == 1) {
+                while (0 != HelpF(stackO.get())) {
+                    out += stackO.get();
+                    out += ' ';
+                    stackO.pop();
+                }
+                stackO.pop();
+            }
+            else if (HelpF(inf[i]) <= HelpF(stackO.get())) {
+                while (!stackO.isEmpty() && 1 < HelpF(stackO.get())) {
+                    out += stackO.get();
+                    out += ' ';
+                    stackO.pop();
+                }
+                stackO.push(inf[i]);
+            }
         }
-        stack.push(inf[i]);
-      }
     }
-  }
-  while (!stack.isEmpty()) {
-    output += stack.get();
-    output += ' ';
-    stack.pop();
-  }
-  output.pop_back();
-  return output;
+    while (stackO.isEmpty() == false) {
+        out += stackO.get();
+        out += ' ';
+        stackO.pop();
+    }
+    out.pop_back();
+    return out;
 }
+
 int eval(std::string pref) {
-  TStack<int, 100> istack;
-  std::string output;
-  int result = 0;
-  int i = 0;
-  for (; i < pref.length(); i++) {
-    std::string check = "";
-    while (5 == pr(pref[i])) {
-      check += pref[i];
-      i += 1;
+    TStack<int, 100> stackT;
+    std::string output;
+    int c = 0;
+    for (int i = 0; i < pref.length(); i++) {
+        std::string check = "";
+        while (5 == pr(pref[i])) {
+            check += pref[i];
+            i += 1;
+        }
+        if (check != "") {
+            stackT.push(std::stoi(check));
+        }
+        if (2 == pr(pref[i]) || 3 == pr(pref[i])) {
+            int F = stackT.get();
+            stackT.pop();
+            int S = stackT.get();
+            stackT.pop();
+            if ('-' == pref[i]) {
+                c = S - F;
+                stackT.push(c);
+            }
+            else if ('+' == pref[i]) {
+                c = F + S;
+                stackT.push(c);
+            }
+            else if ('*' == pref[i]) {
+                c = F * S;
+                stackT.push(c);
+            }
+            else if ('/' == pref[i]) {
+                c = S / F;
+                stackT.push(c);
+            }
+        }
     }
-    if (check != "") {
-      istack.push(std::stoi(check));
-    }
-    if (2 == pr(pref[i]) || 3 == pr(pref[i])) {
-      int a = istack.get();
-      istack.pop();
-      int b = istack.get();
-      istack.pop();
-      if ('-' == pref[i]) {
-        result = b - a;
-        istack.push(result);
-      } else if ('+' == pref[i]) {
-        result = a + b;
-        istack.push(result);
-      } else if ('*' == pref[i]) {
-        result = a * b;
-        istack.push(result);
-      } else if ('/' == pref[i]) {
-        result = b / a;
-        istack.push(result);
-      }
-    }
-  }
-  result = istack.get();
-  return result;
+    return stackT.get();
 }
